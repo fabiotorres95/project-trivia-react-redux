972f8f215108cde979b8c9838eeaba7c0f1faf43
Aline <alinesresende14@gmail.com>
2023-03-21 20:26:11 -0300

    Questão 11

--- src/Routes.js
+++ src/Routes.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Login from './pages/Login';
-import Game from './pages/Game';
 import Config from './pages/Config';
+import Feedback from './pages/Feedback';
+import Game from './pages/Game';
+import Login from './pages/Login';
 
 export default class Routes extends Component {
   render() {
@@ -11,6 +12,7 @@ export default class Routes extends Component {
         <Route exact path="/" component={ Login } />
         <Route path="/game" component={ Game } />
         <Route path="/config" component={ Config } />
+        <Route path="/feedback" component={ Feedback } />
       </Switch>
     );
   }
