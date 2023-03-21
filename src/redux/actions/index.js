a07ff3b9e9e94e5e67ac3525f2fa0b7ac4601d02
Wilson Maximo <maximortywil@gmail.com>
2023-03-21 18:31:57 -0300

    Questão 9

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,7 +1,22 @@
 export const USER_PLAYER = 'USER_PLAYER';
+export const NEXT_QUESTION = 'NEXT_QUESTION';
+export const CORRECT_ANSWER = 'CORRECT_ANSWER';
 
 export const requestUserPlayer = (name, email) => ({
   type: USER_PLAYER,
   name,
   email,
+});
+
+export const requestNextQuestion = (timer, difficulty) => {
+  const initialPoints = 10;
+  const totalScore = initialPoints + (timer * difficulty);
+  return {
+    type: NEXT_QUESTION,
+    score: totalScore,
+  };
+};
+
+export const requestCorrectAnswer = () => ({
+  type: CORRECT_ANSWER,
 });
