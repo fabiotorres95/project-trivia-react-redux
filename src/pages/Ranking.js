fb6452e755cce93c3c62d9139e2113b67d8391ab
Wilson Maximo <maximortywil@gmail.com>
2023-03-22 20:41:42 -0300

    Questão 18

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Ranking extends Component {
   render() {
+    const { history } = this.props;
     return (
-      <div
-        data-testid="ranking-title"
-      >
-        Ranking
+      <>
+        <div
+          data-testid="ranking-title"
+        >
+          Ranking
 
-      </div>
+        </div>
+        <button
+          data-testid="btn-go-home"
+          onClick={ () => history.push('/') }
+        >
+          Home
+        </button>
+      </>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default Ranking;
